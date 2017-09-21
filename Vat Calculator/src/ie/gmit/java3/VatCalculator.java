4d659a41db8837da11f3dbeeb5945c633500904f
g00316210@gmit.ie <stefan@outlook.ie>
2017-09-21 10:53:14 +0100

    extra modifications for testing....

--- Vat Calculator/src/ie/gmit/java3/VatCalculator.java
+++ Vat Calculator/src/ie/gmit/java3/VatCalculator.java
@@ -4,8 +4,8 @@ package ie.gmit.java3;
 import java.util.Scanner;
 public class VatCalculator {
     public static void main(String[] args) {
-    //declare your variables and input your code
-    Scanner console = new Scanner(System.in);
+        //declare your variables and input your code
+        Scanner console = new Scanner(System.in);
         double vatValue;
         double removeVat;
         double addVat;
@@ -15,7 +15,7 @@ public class VatCalculator {
         System.out.println("Please select\n 1 to add VAT or 2 to remove VAT");
         
         choice = console.nextInt();
-    
+        
         switch (choice){
             case 1:
                 System.out.println("You have selected option 1");
@@ -24,19 +24,23 @@ public class VatCalculator {
                 System.out.println(" You have entered " + addVat + " Euro.");
                 addVat = (addVat + addVat * 0.23);
                 System.out.println("The value plus vat is: " + addVat);
-            
+                
                 break;
             case 2:
                 System.out.println("You have selected option 2");
                 System.out.print("Please enter the amount of Euro: ");
+                addVat = console.nextDouble();
+                System.out.println(" You have entered " + addVat + " Euro.");
+                addVat = (addVat - addVat * 0.23);
+                System.out.println("The value with vat deducted is: " + addVat);
                 break;
             default:
                 System.out.println("Must make a proper choice. 1 or 2");
                 break;
         }
-    
-    
-    
+        
+        
+        
     }//end of main
-
+    
 }//vat calculator
